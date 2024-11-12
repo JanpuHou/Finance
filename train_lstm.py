8e46e17a20cb83f4acc68fe759e15b436e61dd71
Janpu Hou <JanpuHou@users.noreply.github.com>
2024-11-12 10:59:58 +0800

    Add files via upload

---
+++ train_lstm.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Fetch historical stock data
+symbol = '2883.tw'
+data = yf.download(symbol, start='2023-11-08', end='2024-11-08', progress=False)
+
+# Display the first few rows of the dataset
+print(data.head())
+data.to_csv(f'2883_data.csv')
+# Read the CSV file
+data = pd.read_csv("2883_data.csv")
+print(data.head())
+plt.plot(data['Close'])
+plt.show()
+
+# We slice the data frame to get the column we want and normalize the data.
+
+from sklearn.preprocessing import MinMaxScaler
+price = data[['Close']]
+scaler = MinMaxScaler(feature_range=(-1, 1))
+price['Close'] = scaler.fit_transform(price['Close'].values.reshape(-1,1))
+
+# Now we split the data into train and test sets. 
+# Before doing so, we must define the window width of the analysis. 
+# The use of prior time steps to predict the next time step is called the sliding window method.
+
+def split_data(stock, lookback):
+    data_raw = stock.to_numpy() # convert to numpy array
+    data = []
+    
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - lookback): 
+        data.append(data_raw[index: index + lookback])
+    
+    data = np.array(data);
+    test_set_size = int(np.round(0.2*data.shape[0]));
+    train_set_size = data.shape[0] - (test_set_size);
+    
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
+    
+    x_test = data[train_set_size:,:-1]
+    y_test = data[train_set_size:,-1,:]
+    
+    return [x_train, y_train, x_test, y_test]
+lookback = 20 # choose sequence length
+x_train, y_train, x_test, y_test = split_data(price, lookback)
+
+print('shape of x_train, y_train, x_test, y_test')
+print (x_test)
+print (y_test)
+
+# Then we transform them into tensors, 
+# which is the basic structure for building a PyTorch model.
+
+import torch
+import torch.nn as nn
+x_train = torch.from_numpy(x_train).type(torch.Tensor)
+x_test = torch.from_numpy(x_test).type(torch.Tensor)
+y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor)
+
+
+# We define some common values for both models regarding the layers.
+
+input_dim = 1
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+num_epochs = 100
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :]) 
+        return out
+
+# We create the model, set the criterion, and the optimiser.
+
+model = LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+criterion = torch.nn.MSELoss(reduction='mean')
+optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# Finally, we train the model over 100 epochs.
+
+import time
+hist = np.zeros(num_epochs)
+start_time = time.time()
+lstm = []
+for t in range(num_epochs):
+    y_train_pred = model(x_train)
+    loss = criterion(y_train_pred, y_train_lstm)
+    print("Epoch ", t, "MSE: ", loss.item())
+    hist[t] = loss.item()
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+    
+training_time = time.time()-start_time
+print("Training time: {}".format(training_time))
+
+predict = pd.DataFrame(scaler.inverse_transform(y_train_pred.detach().numpy()))
+original = pd.DataFrame(scaler.inverse_transform(y_train_lstm.detach().numpy()))
+
+import seaborn as sns
+sns.set_style("darkgrid")    
+
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.2, wspace=0.2)
+
+plt.subplot(1, 2, 1)
+ax = sns.lineplot(x = original.index, y = original[0], label="Data", color='royalblue')
+ax = sns.lineplot(x = predict.index, y = predict[0], label="Training Prediction (LSTM)", color='tomato')
+ax.set_title('Stock price', size = 14, fontweight='bold')
+ax.set_xlabel("Days", size = 14)
+ax.set_ylabel("Cost (USD)", size = 14)
+ax.set_xticklabels('', size=10)
+
+
+plt.subplot(1, 2, 2)
+ax = sns.lineplot(data=hist, color='royalblue')
+ax.set_xlabel("Epoch", size = 14)
+ax.set_ylabel("Loss", size = 14)
+ax.set_title("Training Loss", size = 14, fontweight='bold')
+fig.set_figheight(6)
+fig.set_figwidth(16)
+plt.show()
+
+model_scripted = torch.jit.script(model) # Export to TorchScript
+model_scripted.save('model_scripted.pt') # Save
+
+model = torch.jit.load('model_scripted.pt')
+model.eval()
